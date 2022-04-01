93de285bbf717fbca2b08f2617e3c11befa77ed0
DaveBrun94 <david.brunner@desy.de>
2022-04-02 00:27:28 +0200

    Prefire weights

--- Analyzer/interface/weightanalyzer.h
+++ Analyzer/interface/weightanalyzer.h
@@ -9,7 +9,7 @@ template <typename T>
 class WeightAnalyzer : public BaseAnalyzer<T> {
     private:
         std::string run, era, pileUpFile;
-        TParameter<float> lumi, lumiUp, lumiDown, xSec;
+        TParameter<float> lumi, lumiUp, lumiDown, xSec, xSecUp, xSecDown;
         float nGen;
 
         std::shared_ptr<TH1F> puMC;
@@ -29,6 +29,8 @@ class WeightAnalyzer : public BaseAnalyzer<T> {
             lumiUp = TParameter<float>("LumiUp", skim.get<float>("Analyzer.Weight.Lumi.Up." + era)*1e3);
             lumiDown = TParameter<float>("LumiDown", skim.get<float>("Analyzer.Weight.Lumi.Down." + era)*1e3);
             xSec  = TParameter<float>("xSec", skim.get<float>("xSec"));
+            xSecUp  = TParameter<float>("xSecUp", skim.get<float>("xSec") + skim.get<float>("xSecUnc"));
+            xSecDown  = TParameter<float>("xSecDown", skim.get<float>("xSec") - skim.get<float>("xSecUnc"));
 
             nGen = 0;
 
@@ -49,6 +51,10 @@ class WeightAnalyzer : public BaseAnalyzer<T> {
 
                 puMC->Fill(out.nTrueInt);
             }
+            
+            out.preFire = input.preFire;
+            out.preFireUp = input.preFireUp;
+            out.preFireDown = input.preFireDown;
         }
 
         void EndJob(const std::shared_ptr<TFile>& outFile){
@@ -71,6 +77,8 @@ class WeightAnalyzer : public BaseAnalyzer<T> {
 
                 TParameter<float>("nGen", nGen).Write();
                 xSec.Write();
+                xSecUp.Write();
+                xSecDown.Write();
                 lumi.Write();
                 lumiUp.Write();
                 lumiDown.Write();
