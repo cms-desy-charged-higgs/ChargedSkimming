2f6356ddbb10a021b89b06b89836bb5b9e6d1112
DaveBrun94 <david.brunner@desy.de>
2022-04-02 00:25:09 +0200

    Shape systs as branches

--- Analyzer/interface/electronanalyzer.h
+++ Analyzer/interface/electronanalyzer.h
@@ -9,6 +9,7 @@ template <typename T>
 class ElectronAnalyzer : public BaseAnalyzer<T> {
     private:
         //Era information
+        bool isData;
         std::string era;
 
         //Kinematic cut criteria
@@ -19,13 +20,11 @@ class ElectronAnalyzer : public BaseAnalyzer<T> {
         std::string shift;
 
     public:
-        ElectronAnalyzer(const std::string& systematic, const std::string& shift) : shift(shift){
-            isScaleSyst = systematic.find("eleEnergyScale") != std::string::npos;
-            isSigmaSyst = systematic.find("eleEnergySigma") != std::string::npos;
-        }
+        ElectronAnalyzer(){}
 
         void BeginJob(const pt::ptree& skim, const pt::ptree& sf){
             //Read information needed
+            isData = skim.get<std::string>("run") != "MC";
             era = skim.get<std::string>("era");
 
             ptCut = skim.get<float>("Analyzer.Electron.pt." + era);
@@ -35,31 +34,63 @@ class ElectronAnalyzer : public BaseAnalyzer<T> {
         void Analyze(T& input, Output& out){
             out.nElectrons = 0;
             input.ReadEleEntry();
+            if(!isData) input.ReadGenEntry();
         
             //Loop over all electrons
             for(int i = 0; i < input.eleSize; ++i){
                 if(out.nElectrons >= eleMax) break;
                 input.GetElectron(i);
 
-                float elePt;
-
-                if(isScaleSyst){
-                    elePt = shift == "Up" ? input.elePtScaleUp : input.elePtScaleDown;
-                }
-
-                else if(isSigmaSyst){
-                    elePt = shift == "Up" ? input.elePtSigmaUp : input.elePtSigmaDown;
-                }
-
-                else{
-                    elePt = input.elePt;
+                //Check if one pt value passed pt criteria
+                bool ptCriteria = false;
+        
+                for(const float pt : {input.elePt, input.elePtScaleDown, input.elePtSigmaDown, input.elePtScaleUp, input.elePtSigmaUp}){
+                    if(pt > ptCut){
+                        ptCriteria = true;
+                        break;
+                    }
                 }
               
-                if(elePt > ptCut && std::abs(input.eleEta) < etaCut && input.eleConvVeto){
-                    out.elePt[out.nElectrons] = elePt;
+                if(ptCriteria && std::abs(input.eleEta) < etaCut && input.eleConvVeto){
+                    if(!isData){
+                        int genIdx = input.GenMatch(input, input.elePt, input.elePhi, input.eleEta, 11, 0.4, 0.4);
+
+                        if(genIdx != -1){
+                            input.alreadyMatchedIdx.push_back(genIdx);
+
+                            input.GetGenPart(genIdx);
+                            out.eleGenPt[out.nElectrons] = input.genPt;
+                            out.eleGenPhi[out.nElectrons] = input.genPhi;
+                            out.eleGenEta[out.nElectrons] = input.genEta;
+                            out.eleGenID[out.nElectrons] = input.genPDG;
+
+                            input.GetGenPart(input.genMotherIdx);
+                            out.eleGenMotherID[out.nElectrons] = input.genPDG;
+
+                            input.GetGenPart(input.genMotherIdx);
+                            out.eleGenGrandMotherID[out.nElectrons] = input.genPDG;
+                        }
+
+                        else{
+                            out.eleGenPt[out.nElectrons] = -999.;
+                            out.eleGenPhi[out.nElectrons] = -999.;
+                            out.eleGenEta[out.nElectrons] = -999.;
+                            out.eleGenID[out.nElectrons] = -999;
+                            out.eleGenMotherID[out.nElectrons] = -999;
+                            out.eleGenGrandMotherID[out.nElectrons] = -999;
+                        }
+                    }
+
+                    out.elePt[out.nElectrons] = input.elePt;
+                    out.elePtEnergyScaleUp[out.nElectrons] = input.elePtScaleUp;
+                    out.elePtEnergyScaleDown[out.nElectrons] = input.elePtScaleDown;
+                    out.elePtEnergySigmaUp[out.nElectrons] = input.elePtSigmaUp;
+                    out.elePtEnergySigmaDown[out.nElectrons] = input.elePtSigmaDown;
+                    out.elePt[out.nElectrons] = input.elePt;
+                    out.elePt[out.nElectrons] = input.elePt;
                     out.elePhi[out.nElectrons] =  input.elePhi;
                     out.eleEta[out.nElectrons] =  input.eleEta;
-                    out.eleIso[out.nElectrons] =  input.eleIso;
+                    out.eleIso03[out.nElectrons] =  input.eleIso03;
                     out.eleMiniIso[out.nElectrons] =  input.eleMiniIso;
                     out.eleDxy[out.nElectrons] =  input.eleDxy;
                     out.eleDz[out.nElectrons] =  input.eleDz;
